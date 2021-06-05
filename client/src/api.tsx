362f68e071de522e85dddb17e7cdb6f208ccc36d
Ruben Paredes <ruben.pardes25@gmail.com>
2021-06-04 21:39:18 -0400

    Send authorization header

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-// const baseURL = process.env.NODE_ENV === 'development' ? 'http://192.168.1.13:4000' : process.env.REACT_APP_API_URL;
-
 export default axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {'Authorization': 'Bearer ' + process.env.REACT_APP_SERVER_AUTH_TOKEN }
 })
