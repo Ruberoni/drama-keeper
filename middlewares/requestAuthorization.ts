b37a88ddd6c838120907772fb0cf60a9fed84053
Ruben Paredes <ruben.pardes25@gmail.com>
2021-06-06 19:00:54 -0400

    Server authorization header

--- middlewares/requestAuthorization.ts
+++ middlewares/requestAuthorization.ts
@@ -3,11 +3,11 @@ import { Request, Response, NextFunction } from 'express'
 export default function requestAuthorization(req: Request, res: Response, next: NextFunction) {
   if (process.env.NODE_ENV === 'production') {
     try {
-      const requestAuthHeader = req.get('authorization')
-      if (!requestAuthHeader) throw new Error("Please provide the authorization header.")
+      const requestAuthHeader = req.get('server-authorization')
+      if (!requestAuthHeader) throw new Error("Please provide the server-authorization header.")
       
       const requestToken = requestAuthHeader.replace(/^Bearer\s+/, "")
-      if (!requestToken) throw new Error("Please provide the authorization header.")
+      if (!requestToken) throw new Error("Please provide the server-authorization header.")
 
       if (!process.env.SERVER_AUTH_TOKEN) throw new Error("Please provide the SERVER_AUTH_TOKEN env variable.")
       if (process.env.SERVER_AUTH_TOKEN === requestToken){
