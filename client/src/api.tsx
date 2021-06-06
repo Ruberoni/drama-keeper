66d3389e8b1cd921ad7a0fa5d22794ea3dc66ab5
Ruben Paredes <ruben.pardes25@gmail.com>
2021-06-06 19:04:10 -0400

    Send server authorization header client side

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  headers: {'Authorization': 'Bearer ' + process.env.REACT_APP_SERVER_AUTH_TOKEN }
+  headers: {'server-authorization': 'Bearer ' + process.env.REACT_APP_SERVER_AUTH_TOKEN }
 })
